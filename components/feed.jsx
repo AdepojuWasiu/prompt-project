cd00f743c6d740215108717923b91024a8c45246
AdepojuWasiu <adepojuwasiu031@gmail.com>
2024-03-09 16:48:51 -0800

    fix typo

--- components/feed.jsx
+++ components/feed.jsx
@@ -28,7 +28,7 @@ const Feed = () => {
     
     useEffect( () => {
         const fetchPosts = async () => {
-            const response = await fetch('/api/prompt', {next: {revalidate: 10}});
+            const response = await fetch('/api/prompt', {cache: 'no-store'});
             const data = await response.json();
 
             setPosts(data);
